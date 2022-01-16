bcea4d4837e4b90d388c178c106ec1c3e468d58e
Gourav Jena <gourav.jena2@gmail.com>
2022-01-16 19:26:14 +0530

    Added send_sms.js

---
+++ send_sms.js
@@ -0,0 +1,15 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
+  
+  //accountSid is the unique AccountSID from your twilio account
+  //authToken is the unique Account Authorization Token from your twilio account
+  //Since these informations are critical, they are stored as environmental variables.
