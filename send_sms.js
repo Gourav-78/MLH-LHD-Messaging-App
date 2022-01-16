26a6f35ef820882326cc1eaeb92edb9a44acc5b1
Gourav Jena <gourav.jena2@gmail.com>
2022-01-16 19:29:59 +0530

    Updated send_sms.js

--- send_sms.js
+++ send_sms.js
@@ -10,6 +10,9 @@ client.messages
    })
   .then(message => console.log(message.sid));
   
-  //accountSid is the unique AccountSID from your twilio account
-  //authToken is the unique Account Authorization Token from your twilio account
-  //Since these informations are critical, they are stored as environmental variables.
+//accountSid is the unique AccountSID from your twilio account
+//authToken is the unique Account Authorization Token from your twilio account
+//Since these informations are critical, they are stored as environmental variables.
+//Replace the existing 'from' number with that one you bought from twilio
+//Replace the existing 'to' number with the one from verified caller-ids in your twilio account
+//'body' is the actual message
